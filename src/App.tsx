9c260e996e7199d3d12ca0821fdf333c74fe4b5d
albrimpaqarzi <albrim.paqarizi@ornio.no>
2022-04-12 14:35:28 +0200

    main Updated eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Button } from 'antd';
 import logo from './assets/logo.svg';
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -40,6 +40,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
